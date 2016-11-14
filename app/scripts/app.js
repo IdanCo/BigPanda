39a80f1a0769fbc733e5348f2c6bc0a0d47560db
Idan Cohen <idan@idancohen.com>
2016-11-14 22:05:30 +0200

    clean slate

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name pandaApp
+ * @description
+ * # pandaApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('pandaApp', [
+    'ngAnimate'
+  ]);
